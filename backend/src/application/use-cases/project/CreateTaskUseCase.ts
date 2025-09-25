6b595064abd3f37cc1aef28463b9d246d16de204
Amruth Shyju <amruthshyju@gmail.com>
2025-09-25 09:50:20 +0530

    refactor:Updated the task usecase

--- backend/src/application/use-cases/project/CreateTaskUseCase.ts
+++ backend/src/application/use-cases/project/CreateTaskUseCase.ts
@@ -1,13 +1,37 @@
 import { Task } from "../../../domain/entities/Task";
+import { IEmployeeRepository } from "../../../domain/repositories/IEmployeeRepository";
 import { ITaskRepository } from "../../../domain/repositories/ITaskRepository";
+import { IUserStoryRepository } from "../../../domain/repositories/IUserStoryRepository";
+import { AppError } from "../../../interfaces/middleware/ErrorMiddleware";
 import { CreateTaskDTO } from "../../dto/project/CreateTaskDTO";
 import { ICreateTaskUseCase } from "../../interfaces/project/ICreateTaskUseCase";
 
 export class CreateTaskUseCase implements ICreateTaskUseCase {
-  constructor(private readonly _taskRepo: ITaskRepository) {}
+  constructor(
+    private readonly _taskRepo: ITaskRepository,
+    private readonly _userStoryRepo: IUserStoryRepository,
+    private readonly _employeeRepo: IEmployeeRepository
+  ) {}
 
   async execute(taskDTO: CreateTaskDTO): Promise<Task> {
   
+    const userStory = await this._userStoryRepo.findById(taskDTO.userStoryId);
+    if (!userStory) throw new AppError("UserStory not found", 404);
+
+    
+ if (taskDTO.assignedToId) {
+      const employee = await this._employeeRepo.findById(taskDTO.assignedToId);
+      if (!employee) {
+        throw new AppError(`Assigned user with ID ${taskDTO.assignedToId} does not exist`, 404);
+      }
+
+      if (employee.companyId !== userStory.projectId) { 
+        throw new AppError(`Assigned user does not belong to the same company as the user story`, 400);
+      }
+    }
+
+    
+    
 
     const task = new Task(
       undefined,                
