d7698b50b344ca1c8221f94b3123d628ac4ed3d5
Amruth Shyju <amruthshyju@gmail.com>
2025-10-11 09:45:33 +0530

    fix:employee adding added

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -59,7 +59,7 @@ export const PROJECT_ROUTES = {
   CREATE_ISSUE:"/api/project/create-issue",
   GET_EMPLOYEE_UNDER_PROJECT: (id: string) => `/api/project/issuelevel-allcated-employee/${id}`,
   GET_DEPARTMENT_EMPLOYEES:"/api/project/projectlevel-allocated-employee",
-  GET_EMPLOYEE_NOT_IN_PROJECT: (id: string) =>`/api/project/employee-out-project/${id}`
+  GET_EMPLOYEE_NOT_IN_PROJECT: (id: string) =>`/api/project/employee-out-project/${id}`,
   Add_EMPLOYEE_TO_PROJECT:'/api/project/add-employee-project'
 
 }
