10a19278990e93984b1a05c96e31ebb5a381527f
Amruth Shyju <amruthshyju@gmail.com>
2025-11-07 17:22:59 +0530

    feat:implemented the get notification in backend

---
+++ backend/src/infrastructure/socket/NotificationSocket.ts
@@ -0,0 +1,47 @@
+import { Server } from "socket.io";
+import { Server as HttpServer } from "http";
+
+
+
+const connectedUsers = new Map<string, string>();
+
+
+export const initSocket = (server: HttpServer) => {
+  const io = new Server(server, {
+    cors: {
+      origin: "*", 
+    },
+  });
+
+
+   io.on("connection", (socket) => {
+    console.log("Socket connected:", socket.id);
+
+    socket.on("register", (userId: string) => {
+      connectedUsers.set(userId, socket.id);
+      console.log(`User ${userId} connected`);
+    });
+
+    socket.on("disconnect", () => {
+      for (const [userId, id] of connectedUsers.entries()) {
+        if (id === socket.id) {
+          connectedUsers.delete(userId);
+          console.log(`User ${userId} disconnected`);
+          break;
+        }
+      }
+    });
+  });
+  return io;
+}
+
+
+export const emitNotification = (io: Server, userId: string, notification: unknown) => {
+  const socketId = connectedUsers.get(userId);
+  if (socketId) {
+    io.to(socketId).emit("new-notification", notification);
+    console.log("new Notification",notification);
+    
+    console.log(`Sent notification to user ${userId}`);
+  }
+};
